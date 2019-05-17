4de50c169f0c12e235e2ed2249a937f5bfd92e85
Scott Huffman <scott@Scotts-MacBook-Pro.local>
2019-05-17 11:44:19 -0500

    Moved css page transitions into the page switcher.

--- src/PageSwitcher.js
+++ src/PageSwitcher.js
@@ -1,20 +1,81 @@
-module.exports = function(homePageId, homeLinkId) {
+// transition options: bottom, top, left, right, fade
+module.exports = function(homePageId, homeLinkId, transition = 'fade') {
     const app = {
         pages: [],
         activeState: null,
     };
 
-    const createPage = (pageId, linkId) => {
+    const element = document.createElement('style');
+    document.head.appendChild(element);
+    const stylesheet = element.sheet;
+
+    stylesheet.insertRule(`
+        .pageSwitcher.-pageSwitcherActive{
+            transform: translateY(0) translateX(0);
+            opacity: 1;
+        }`);
+    stylesheet.insertRule(`
+        .pageSwitcher.-pageSwitcherleft {
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            transition: 1s;
+            top: 0;
+            transform: translateX(-100vw);
+        }`);
+
+    stylesheet.insertRule(`
+        .pageSwitcher.-pageSwitcherright {
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            transition: 1s;
+            top: 0;
+            transform: translateX(100vw);
+        }`);
+
+    stylesheet.insertRule(`
+        .pageSwitcher.-pageSwitchertop {
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            transition: 1s;
+            top: 0;
+            transform: translateY(-100vh);
+        }`);
+    stylesheet.insertRule(`
+        .pageSwitcher.-pageSwitcherbottom {
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            transition: 1s;
+            top: 0;
+            transform: translateY(100vh);
+        }`);
+    stylesheet.insertRule(`
+        .pageSwitcher.-pageSwitcherfade {
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            transition: 1s;
+            top: 0;
+            opacity: 0;
+        }`);
+
+    console.log(stylesheet);
+    const createPage = (pageId, linkId, transition) => {
         const page = document.getElementById(pageId);
         const link = document.getElementById(linkId);
+        page.classList.add('pageSwitcher');
+        page.classList.add(`-pageSwitcher${transition}`);
 
         const state = {
             from: () => {
-                page.classList.remove('-active');
+                page.classList.remove('-pageSwitcherActive');
                 link.classList.remove('-active');
             },
             to: () => {
-                page.classList.add('-active');
+                page.classList.add('-pageSwitcherActive');
                 link.classList.add('-active');
             },
         };
@@ -26,8 +87,8 @@ module.exports = function(homePageId, homeLinkId) {
         return state;
     };
 
-    app.addPage = (pageId, linkId) => {
-        app.pages.push(createPage(pageId, linkId));
+    app.addPage = (pageId, linkId, transition='fade') => {
+        app.pages.push(createPage(pageId, linkId, transition));
     };
 
     app.transition = (to) => {
@@ -36,7 +97,7 @@ module.exports = function(homePageId, homeLinkId) {
         app.activeState.to();
     };
 
-    app.activeState = createPage(homePageId, homeLinkId);
+    app.activeState = createPage(homePageId, homeLinkId, transition);
     app.pages.push(app.activeState);
     app.activeState.to();
 
--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const PageSwitcher = require('./PageSwitcher.js');
 
 document.addEventListener('DOMContentLoaded', () => {
     const pages = new PageSwitcher('home-page', 'home-link');
-    pages.addPage('page-1', 'page1-link');
-    pages.addPage('page-2', 'page2-link');
+    pages.addPage('page-1', 'page1-link', 'left');
+    pages.addPage('page-2', 'page2-link', 'right');
 });
